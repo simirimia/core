c4bf8f51bdb67d5e9f16208ea39ea51b0162ce32
Verena Ruff <verena.ruff@flyeralarm.com>
2015-06-20 13:59:56 +0200

    simple json result class without any checks

---
+++ Simirimia/Core/src/Result/JsonResult.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v.ruff
+ * Date: 20.06.2015
+ * Time: 13:54
+ */
+
+namespace Simirimia\Core\Result;
+
+
+class JsonResult {
+
+}
---
+++ Simirimia/Core/src/ResultRenderer/JsonResultRenderer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v.ruff
+ * Date: 20.06.2015
+ * Time: 13:56
+ */
+
+namespace Simirimia\Core\ResultRenderer;
+
+
+class JsonResultRenderer {
+
+}
