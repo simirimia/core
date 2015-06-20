1d234b28a47851edc07fe7f5cf5d1ba1354bfce2
Verena Ruff <verena.ruff@flyeralarm.com>
2015-06-20 14:03:52 +0200

    some stuff was missing

--- Simirimia/Core/src/Result/JsonResult.php
+++ Simirimia/Core/src/Result/JsonResult.php
@@ -1,14 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: v.ruff
- * Date: 20.06.2015
- * Time: 13:54
+/*
+ * This file is part of the simirimia/core package.
+ *
+ * (c) https://github.com/simirimia
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Simirimia\Core\Result;
 
 
-class JsonResult {
+class JsonResult implements Result
+{
+    use ResultCode;
 
+    /**
+     * @var string
+     */
+    private $data;
+
+    public function __construct( $jsonString, $resultCode = null )
+    {
+        $this->data = $jsonString;
+        if ( null !== $resultCode ) {
+            if ( is_int( $resultCode ) ) {
+                $this->setResultCode( $resultCode );
+            } else {
+                throw new \InvalidArgumentException( '$resultCode needs to be integer or omitted' );
+            }
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getJsonString()
+    {
+        return $this->data;
+    }
 }
--- Simirimia/Core/src/ResultRenderer/JsonResultRenderer.php
+++ Simirimia/Core/src/ResultRenderer/JsonResultRenderer.php
@@ -1,14 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: v.ruff
- * Date: 20.06.2015
- * Time: 13:56
+/*
+ * This file is part of the simirimia/core package.
+ *
+ * (c) https://github.com/simirimia
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Simirimia\Core\ResultRenderer;
 
+use Simirimia\Core\Result\JsonResult;
 
-class JsonResultRenderer {
+class JsonResultRenderer implements ResultRenderer
+{
+
+    public static function render( $result )
+    {
+        if ( !( $result instanceof JsonResult ) ) {
+            throw new \InvalidArgumentException( '$result needs to be of type JsonResult' );
+        }
+
+        return $result;
+    }
 
 }
