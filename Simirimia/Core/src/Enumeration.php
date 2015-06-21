7518239e2a3f6bbc09db0d4d5bb63c9171f440ac
Verena Ruff <verena.ruff@flyeralarm.com>
2015-06-21 15:23:57 +0200

    Enumeration without SPL

--- Simirimia/Core/src/Enumeration.php
+++ Simirimia/Core/src/Enumeration.php
@@ -10,19 +10,74 @@
 
 namespace Simirimia\Core;
 
-use \SplEnum;
+use \ReflectionClass;
+use \InvalidArgumentException;
 
-abstract class Enumeration extends SplEnum
+abstract class Enumeration
 {
+    const __default = null ;
+
+    /**
+     * @var mixed
+     */
+    private $value;
+
+    /**
+     * @param mixed $initialValue
+     */
+    public function __construct ( $initialValue = null )
+    {
+        if ( in_array( $initialValue, $this->getConstList( true ) )) {
+            $this->value = $initialValue;
+        } elseif( $initialValue === null ) {
+            $this->value = constant( get_called_class() . '::__default' );
+        } else {
+            throw new InvalidArgumentException( 'Invalid value: ' . $initialValue );
+        }
+    }
+
+    /**
+     * @param bool $includeDefault
+     * @return array
+     */
+    public function getConstList ( $includeDefault = false )
+    {
+        $reflectionClass = new ReflectionClass( get_called_class() );
+        $constants = $reflectionClass->getConstants();
+        if ( ! $includeDefault ) {
+            unset( $constants['__default'] );
+        }
+        return $constants;
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
     public static function hasKey( $key )
     {
         try {
             $enumClassName = get_called_class();
-            new $enumClassName($key);
+            new $enumClassName( $key );
             return true;
-        } catch ( \Exception $e ) {
+        } catch ( InvalidArgumentException $e ) {
             return false;
         }
     }
 
+    /**
+     * @return string
+     */
+    public function getValuesName()
+    {
+        return array_search( $this->value, $this->getConstList() );
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string)$this->value;
+    }
 }
